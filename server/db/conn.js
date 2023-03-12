32684c5977975a8fc95e75f769cedd833d837c24
Aditya1482002 <aditya1482002@gmail.com>
2023-03-12 17:03:40 +0530

    comit

--- server/db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-// const url="mongodb+srv://root:root@cluster0.vcvx4oj.mongodb.net/?retryWrites=true&w=majority";
-const url="mongodb+srv://root:root@cluster0.p9eu6zv.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(url).then(()=>{
-    console.log("Database connected");
-}).catch((err)=>{
-    console.log(err);
-})
